cd1ca8014d186d4d789c6aac7f47082893c6b1bd
Max Edwards <youwontforgetthis@gmail.com>
2026-06-06 15:38:28 -0400

    added c++ solution to exercise 1

--- week1/exercise1/fastexponentiation.cpp
+++ week1/exercise1/fastexponentiation.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <format>
+
+
+int fastExponentiation(int base, int exponent) {
+	if (exponent == 0) {
+		return 1;
+	}
+	if (exponent % 2 == 0) {
+		return fastExponentiation(base * base, exponent / 2);
+	}
+	return base * fastExponentiation(base, exponent - 1);
+}
 
 int main() {
 	std::cout << "Welcome to Fast Exponentiation!" << std::endl;
-	int result = -5 % 3;
-	std::cout << result << std::endl;
+	int base, exponent;
+	std::cout << "Enter base: ";
+	std::cin >> base;
+	std::cout << "Enter exponent: ";
+	std::cin >> exponent;
+	int result = fastExponentiation(base, exponent);
+	std::cout << std::format("{} ^ {} = {}", base, exponent, result) << std::endl;
 }
